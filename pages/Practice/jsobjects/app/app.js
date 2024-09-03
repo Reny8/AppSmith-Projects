544932e492ebb38666d127c4e2d841bd725c7ca9
rreyes <rreyes@jwpepper.com>
2024-09-03 15:49:20 +0000

    Update addTodo; Add updateTodo function to app.js

--- pages/Practice/jsobjects/app/app.js
+++ pages/Practice/jsobjects/app/app.js
@@ -1,5 +1,16 @@
 export default {
-	addTodo(todo) {
+	addTodo(title) {
+		let todo = {
+			id: data.todos.length + 1,
+			title: title,
+			completed: false
+		}
 		data.todos.push(todo)
+	},
+
+	updateTodo(id, todo) {
+		data.todos[id] = {
+			...data.todos[id], ...todo
+		}
 	}
 }
